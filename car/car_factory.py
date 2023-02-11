afb502532bb3c9801540d7bea64769877d62e416
emanAtassi <eman.atassi@gmail.com>
2023-02-11 23:29:46 +1100

    changed the methods in CarFactory to static

--- car/car_factory.py
+++ car/car_factory.py
@@ -9,30 +9,35 @@ from datetime import datetime
 
 class CarFactory:
 
+    @staticmethod
     def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(last_service_date, current_date)
         car = Car(engine, battery)
         return car
 
+    @staticmethod
     def create_glissade(self,current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(last_service_date, current_date)
         car = Car(engine, battery)
         return car
 
+    @staticmethod
     def create_palindrome(self,current_date, last_service_date, warning_light_on):
         engine = SternmanEngine(warning_light_on)
         battery = SpindlerBattery(last_service_date, current_date)
         car = Car(engine, battery)
         return car     
     
+    @staticmethod
     def create_rorschach(self,current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(last_service_date, current_date)
         car = Car(engine, battery)
         return car    
 
+    @staticmethod
     def create_thovex(self,current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)       
         battery = NubbinBattery(last_service_date, current_date)
